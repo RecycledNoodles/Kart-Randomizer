e16362c1eaf8b30cdfe279769dfb9cb8f9d584c6
Bao Vuong <vqbao@lv5.org>
2015-01-27 01:00:56 -0500

    added ini file for sql authentication.

--- RandomKartWii/src/main/java/randomkartwii/dao/BaseDAO.java
+++ RandomKartWii/src/main/java/randomkartwii/dao/BaseDAO.java
@@ -1,21 +1,44 @@
 package randomkartwii.dao;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
+
 public class BaseDAO {
 	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	private final String DB_URL = "jdbc:mysql://localhost/randomkartwii";	
-	private final String USER = "root";
-	private final String PASS = "con!rong";
+	
+	private String username = "";
+	private String password = "";
 	
 	protected BaseDAO() {
-		
 		try {
 			Class.forName(JDBC_DRIVER);
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// importing username and password
+		
+		URL path = BaseDAO.class.getResource("/sql/auth.ini");
+		
+		try {
+			System.out.println(path.getFile());
+			Wini authIni = new Wini(new File(path.getFile()));
+			username = authIni.get("mysql", "username", String.class);
+			password = authIni.get("mysql", "password", String.class);
+		} catch (InvalidFileFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -24,7 +47,7 @@ public class BaseDAO {
 	
 	protected Connection createConnection() {
 		try {
-			return (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
+			return (Connection) DriverManager.getConnection(DB_URL, username, password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

df451fd327da0a1af17c21ec6551b9fa2fe657ff
Bao Vuong <vqbao@lv5.org>
2015-02-03 15:03:11 -0500

    l

--- RandomKartWii/src/main/java/randomkartwii/service/RandomKartWiiService.java
+++ RandomKartWii/src/main/java/randomkartwii/service/RandomKartWiiService.java
@@ -57,9 +57,9 @@ public class RandomKartWiiService {
 		connection.setRequestProperty("Accept-Charset", charset);
 		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
 
-		//try (OutputStream output = connection.getOutputStream()) {
-		//    output.write(query.getBytes(charset));
-		//}
+		try (OutputStream output = connection.getOutputStream()) {
+		    output.write(query.getBytes(charset));
+		}
 		
 		//InputStream response = connection.getInputStream();
 		// ...
